201d1e44d6c334ea40de4dd37b9c996d5720915c
Yer Yang <yyhmg_86@yahoo.com>
2023-04-05 03:50:20 -0700

    Creates Function Declaration, Function Expression and Functions-Level Scope

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(doSomething = "roller-skate") {
+    return `This Saturday, I want to ${doSomething}!`
+}
+saturdayFun();
+saturdayFun("hike");
+
+const mondayWork = function (mondayActivity = "go to the office") {
+    return `This Monday, I will ${mondayActivity}.`;
+}
+mondayWork();
+mondayWork("work from home");
+
+function wrapAdjective(star) {
+    return function (adjective1 = "special") {
+        return `You are ${star}${adjective1}${star}!`;
+    }
+}
+wrapAdjective("*")("a dedicated programmer");
